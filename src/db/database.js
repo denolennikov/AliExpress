4cbe7d2744cb241d8a35d1f3fe3cd89beff40b9a
denolennikov <den.olennikov@gmail.com>
2020-01-29 07:16:12 -0800

    renaming database name

--- src/db/database.js
+++ src/db/database.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
                 host     : process.env.MYSQL_HOST,
                 user     : process.env.MYSQL_USER,
                 password : process.env.MYSQL_USER_PASSWORD,
-                database : 'Aliexpress',
+                database : 'aliexpress',
                 debug    : false 
             });
 
